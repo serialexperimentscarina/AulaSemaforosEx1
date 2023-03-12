fb9b7e95dba9ff4acbc6dceb5e346aaa1f005350
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-03-12 10:57:29 -0300

    feat: Principal

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,9 +1,19 @@
 package view;
 
+import java.util.concurrent.Semaphore;
+
+import controller.ThreadCarro;
+
 public class Principal {
 
 	public static void main(String[] args) {
-
+		Semaphore mutex = new Semaphore(1);
+		char[] direcoes = {'N', 'S', 'L', 'O'};
+		
+		for (char direcao : direcoes) {
+			Thread carro = new ThreadCarro(direcao, mutex);
+			carro.start();
+		}
 	}
 
 }
